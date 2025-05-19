253417caa5f7ed84e26251fbf31789f2c003fb6c
Grecia Delgado <greciadm2596@gmail.com>
2025-05-19 12:54:43 -0500

    Add new config to api gatway

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '', method: RequestMethod.GET }],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter());
+  await app.listen(envs.port);
+  console.log('Healh check is running');
+
+  logger.log(`Gateway is running on: ${envs.port}`);
+
+}
+bootstrap();
